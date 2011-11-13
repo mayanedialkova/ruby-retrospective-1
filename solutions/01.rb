978a9babaf22d7b33930df827cc7a9a53e646949
mayanedialkova <mayanedialkova@gmail.com>
2011-11-13 21:47:10 +0200

    01.rb

---
+++ solutions/01.rb
@@ -0,0 +1,26 @@
+class Array
+  def to_hash 
+    self.flatten(1)  
+    Hash[self]
+  end
+ 
+  def index_by
+    indexed = {}
+    self.each {|x| indexed[yield(x)] = x} #?? #ima li nujda ot self.each {...} ??
+    indexed
+  end
+  
+  def subarray_count(subarray)
+    count = 0
+    self.each_cons(subarray.length) {|x| count +=1 if x == subarray}
+    count
+  end    
+      
+  def occurences_count
+    occurences = Hash.new(0)
+    self.map {|x| occurences[x] += 1}
+    occurences
+  end
+end
+
+p [[:one, 1], [:two, 2]].to_hash
