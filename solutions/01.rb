4d48d5bea21719288a67908ef3fbead574edbb48
mayanedialkova <mayanedialkova@gmail.com>
2011-11-14 15:50:32 +0200

    Update solutions/01.rb

--- solutions/01.rb
+++ solutions/01.rb
@@ -1,24 +1,23 @@
 class Array
   def to_hash 
-    self.flatten(1)  
-    Hash[self]
+    Hash[*flatten]
   end
  
   def index_by
     indexed = {}
-    self.each {|x| indexed[yield(x)] = x} #ima li nujda ot self.each {...} ?
+    each {|x| indexed[yield(x)] = x}
     indexed
   end
   
   def subarray_count(subarray)
     count = 0
-    self.each_cons(subarray.length) {|x| count += 1 if x == subarray}
+    each_cons(subarray.length) {|x| count +=1 if x == subarray}
     count
   end    
       
   def occurences_count
     occurences = Hash.new(0)
-    self.map {|x| occurences[x] += 1}
+    map {|x| occurences[x] += 1}
     occurences
   end
 end
